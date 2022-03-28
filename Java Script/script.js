09aecccef30b3e46237826ef207a64066fb68236
Rana Butto/Shuli <ranosh_310@hotmail.com>
2022-03-28 13:15:02 +0300

    Arrays home-work

---
+++ Java Script/script.js
@@ -0,0 +1,86 @@
+// //this is question 1
+// var names=[];
+// for(var i=0; i<5 ;i++){
+//     names[i]=prompt("Enter Your Name");
+// }
+// alert(names[(i=0)]+""+ names[(i=4)]);
+// alert(names[(i=0)]);
+// alert(names[(i=4)]);
+// for(var j=5;j>=0;j--){
+//     names1[j]=prompt("Enter Your Name");
+// }
+// alert(names1[j]);
+//------------------------------------------------------------------------------
+// //this is question 2
+// var fruits=["Mango","Pear","Apple","Banana"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// for(i=0; i<4; i++){
+//     console.log(fruits[i]);
+// }
+//----------------------------------------------------------
+// // //this is question 3
+// var grades=[];
+// for (var i = 0; i < 10; i++); {
+//     grades = +prompt("Enter Your Grade!");
+//     if (grades[i]< 0)   {
+//         alert("This grade Not avaible" + "" + grades[i])
+//     }
+//     else if (grades[i] > 100){
+//         alert("This grade Not avaible" + "" + grades[i])
+//     }
+// }
+//------------------------------------------------------------
+// // //this is question 4
+// var numbers = [6, 85, 74, 96, 100, 78, 45, 12, 58, 1, 28, 81, 4, 15, 22, 10, 3, 88, 56, 29];
+// var rev = 0; sum = 0; avg = 0; max = 0; min = 0;
+// for (i = 19; i >= 0; i--) {
+//     alert(numbers[i]);
+//     sum += numbers[i];
+//     avg = sum / 20;
+//     if (numbers[i] > max) {
+//         max = numbers[i]
+//     }
+//     else if (numbers[i] < min) {
+//         min = numbers[i];
+//     }
+
+// }
+// alert("The Sum of Number is: "+sum);
+// alert("The Avg of number is: "+avg);
+// alert("The Maximum Number is: "+max);
+// alert("The Minemum Numer is: "+min);
+//-------------------------------------------------------------
+//this is question 20
+// var num=[];
+// n=+prompt("Enter Your number");
+// for(var i=1;i<=num;i++){
+//     for(var j=1;j<=num;J++){
+//         alert(j+" ");
+//     }
+
+// }
+//---------------------------------------------------------
+//this is question 21
+// var num=[];
+// num=+prompt("Enter a number");
+// for(var i=1; i<=n; i++){
+// for(j=1;j<=n;j++){
+//    console.log(j+" ")
+// }
+
+//  }
+//----------------------------------------------------
+//this is question 22
+
+// var num = [];
+//     num = +prompt("Enter a number");
+//     if (num > 0)
+//         for (i = num; i >= 1; i--) {
+//             alert(i);
+//         }
+
+
+
---
+++ README/README.md
@@ -0,0 +1,7 @@
+# HOME WORK
+
+## Contains Solves of home work About Arrays
+
+### Have 7 Question
+
+### Contains 2 Folder: HTML and Java Script
